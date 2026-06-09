cac69442807536f30848ec093efa3fb5ec73abe3
Bryan Alpecho <82820720+bcalpecho@users.noreply.github.com>
2026-06-09 17:43:38 +0800

    update ensemble composition

--- scripts/6a_generate_ensemble.R
+++ scripts/6a_generate_ensemble.R
@@ -7,18 +7,11 @@
 # https://github.com/SnBuenafe/hotrstuff.
 # Citation: Buenafe K, Schoeman D, Everett J (2025). hotrstuff: Facilitates the rapid download, wrangling and processing of Earth System Model (ESM) output from the Coupled Model Intercomparison Project (CMIP). R package version 0.0.2, https://github.com/SnBuenafe/hotrstuff.
 
-# install 'hotrstuff' package
-devtools::install_github("SnBuenafe/hotrstuff")
+# #Install package
+# devtools::install_github("SnBuenafe/hotrstuff")
 
-#load packages
-library(hotrstuff)
-
-# define variable
-var <- "chlos"
-
-#List of ESMs used for generating an ensemble
+#List of ESMs used in generating ensembles
 ## ACCESS-ESM1-5
-## CanESM5
 ## CanESM5-1
 ## IPSL-CM6A-LR
 ## CESM2-WACCM
@@ -28,33 +21,91 @@ var <- "chlos"
 ## NorESM2-LM
 ## NorESM2-MM
 
-#01 to download ESM outputs using the following functions (alternatively, manually download from ESGF)
-  base_dir <- "/home/bcalp/UQ" 
-  
+#load packages
+  library(hotrstuff)
+  library(tidyverse)
+
+# define variable
+  var <- "chlos"
+  base_dir <- "/home/bcalp/UQ"
+
+# to download ESM outputs using the following functions (alternatively, manually download from ESGF)
   #prior step: save wget files in the path directory of 'indir' attribute of 'htr_download_ESM'
-  htr_download_ESM(
-    hpc = NA,
-    indir = file.path(base_dir, "data", "wget"), #directory for wget scripts
-    outdir = file.path(base_dir, "data", "raw", var), #directory for raw ESM output
-    quiet = TRUE,
-    security = FALSE)
-  
-#02 to merge files
+  #check wget list
+    wget_list <-   list.files(file.path(base_dir, "data", "wget"))  
+
+  #download
+    htr_download_ESM(
+      hpc = NA,
+      indir = file.path(base_dir, "data", "wget"),
+      outdir = file.path(base_dir, "data", "raw", var))
+
+  #detect experiment
+   #identify ssp scenario
+   ensemble_list <- list.files(file.path(base_dir, "data", "raw", var))  
+   
+   # A. function to detect scenario and assign inputs for later functions
+   detect_esm <- function(ensemble_list){
+     exp_list <- data.frame(id = numeric(length = length(ensemble_list)),
+                            source_id = character(length = length(ensemble_list)),
+                            experiment = character(length = length(ensemble_list)))
+     
+     for(i in 1:length(ensemble_list)){
+       ssp_scenario <- str_extract(basename(ensemble_list[i]), "ssp\\d{3}|historical")
+       exp_list$id[i] <- i
+       exp_list$source_id <- basename(ensemble_list[i])
+       exp_list$experiment[i] <- ssp_scenario
+     }
+     
+     print(exp_list %>% group_by(experiment) %>% summarise(count = n()))
+     
+     exp_lvl_count <- nlevels(factor(exp_list$experiment))
+     
+     if(exp_lvl_count == 1){
+       scenario <- levels(factor(exp_list$experiment))
+       print(paste0("Single experiment detected: ", scenario))
+       
+       assign("ssp_scenario", scenario, envir = .GlobalEnv)
+       if(scenario == "historical"){
+         year_start = "1850"
+         year_end = "2014"
+         assign("year_start", year_start, envir = .GlobalEnv)
+         assign("year_end", year_end, envir = .GlobalEnv)
+       }else if(scenario %in% c("ssp126","ssp245","ssp370","ssp585")){
+         year_start = "2015"
+         year_end = "2100"
+         assign("year_start", year_start, envir = .GlobalEnv)
+         assign("year_end", year_end, envir = .GlobalEnv)
+       }else{
+         print("Experiment cannot be determined")
+       }
+     }else{
+       print("Multiple experiments detected")
+     }
+   }
+   detect_esm(ensemble_list)
+   
+   # B. type in values:
+   # year_start <- "1850"
+   # year_end <- "2014"
+   # scenario <- "historical"
+
+# to merge files
   htr_merge_files(
     indir = file.path(base_dir, "data", "raw", var), # input directory
     outdir = file.path(base_dir, "data", "proc", "merged", var), # output directory
-    year_start = 2015, # earliest year across all the scenarios considered (e.g., historical, ssp126, ssp245, ssp585)
-    year_end = 2100 # latest year across all the scenarios considered
+    year_start = year_start, # earliest year across all the scenarios considered (e.g., historical, ssp126, ssp245, ssp585)
+    year_end = year_end # latest year across all the scenarios considered
   )
 
-# to adjust and reframe time periods to 2015-2100 for ScenarioMIPs and 1980-2000 for historical data of ESMs
+# to adjust and reframe time periods to 2015-2100 for ScenarioMIPs and 1850-2014 for historical data of ESMs
   htr_slice_period(
     indir = file.path(base_dir, "data", "proc", "merged", var), # input directory
     outdir = file.path(base_dir, "data", "proc", "sliced", var), # output directory
-    freq = "Omon", # ocean, monthly
-    scenario = "ssp",
-    year_start = 2015, #
-    year_end = 2100, #double check years depending if ScenarioMIP or historical
+    freq = "Omon", # ocean, daily
+    scenario = ssp_scenario,
+    year_start = year_start,
+    year_end = year_end,
     overwrite = FALSE
   )
 
@@ -75,24 +126,38 @@ var <- "chlos"
     cell_res = 1.0,
     layer = "annual"
   )
+  
+#to generate ensemble
+#ensemble by mean
+  htr_create_ensemble(
+    indir = file.path(base_dir, "data", "proc", "regridded", "yearly", var), # input directory
+    outdir = file.path(base_dir, "data", "proc", "ensemble", "mean", var), # output directory
+    model_list = c("ACCESS-ESM1-5","CanESM5-1","CESM2-WACCM", "CMCC-ESM2","IPSL-CM6A-LR","MPI-ESM1-2-HR","MPI-ESM1-2-LR","NorESM2-LM","NorESM2-MM"), # list of models for ensemble
+    variable = var, # variable name
+    freq = "Omon", # original frequency of data
+    scenario = ssp_scenario, # scenario
+    mean = TRUE # if false, takes the median
+  )
+  # #to plot
+  # ensemble_model <- list.files(file.path(base_dir, "data", "proc", "ensemble", "mean", var), full.names = TRUE)
+  # ensemble <- rast(ensemble_model)
+  # plot(ensemble$chlos_86)
 
-# to generate ensemble
-  #prior step: double check the scenario and model_list for the involved ESMs in the ensemble to be generated.
-  #ensemble by median
+#ensemble by median
   htr_create_ensemble(
     indir = file.path(base_dir, "data", "proc", "regridded", "yearly", var), # input directory
     outdir = file.path(base_dir, "data", "proc", "ensemble", "median", var), # output directory
-    model_list = c("ACCESS-ESM1-5","CanESM5","CanESM5-1","CESM2-WACCM", "CMCC-ESM2","IPSL-CM6A-LR","MPI-ESM1-2-HR","MPI-ESM1-2-LR","NorESM2-LM","NorESM2-MM"), # list of models for ensemble
+    model_list = c("ACCESS-ESM1-5","CanESM5-1","CESM2-WACCM", "CMCC-ESM2","IPSL-CM6A-LR","MPI-ESM1-2-HR","MPI-ESM1-2-LR","NorESM2-LM","NorESM2-MM"), # list of models for ensemble
     variable = var, # variable name
     freq = "Omon", # original frequency of data
-    scenario = "ssp370", # scenario
+    scenario = ssp_scenario, # scenario
     mean = FALSE # if false, takes the median
   )
+  # #to plot
+  # ensemble_model <- list.files(file.path(base_dir, "data", "proc", "ensemble", "median", var), full.names = TRUE)
+  # ensemble <- rast(ensemble_model)
+  # plot(ensemble$chlos_86)
 
-# to check the ensemble 
-  ensemble_model <- list.files(file.path(base_dir, "data", "proc", "ensemble", "median", var), full.names = TRUE)
-  ensemble <- rast(ensemble_model)
-  plot(ensemble$chlos_86)  #to plot ensemble at year 2100 (value of 'time' = 86)
-  
-##
-  
+##End
+#05062026 - added historical scenario
+#09062026 - removed CanESM5; kept CanESM5-1
